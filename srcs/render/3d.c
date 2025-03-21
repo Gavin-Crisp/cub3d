55040aaf0fd866ce10ff0b7f218287f584fa7cc4
gcrisp <gcrisp@F2K010.42adel.org.au>
2025-03-21 11:55:00 +1030

    Sped up rendering

--- srcs/render/3d.c
+++ srcs/render/3d.c
@@ -6,7 +6,7 @@
 /*   By: gcrisp <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 12:36:58 by gcrisp            #+#    #+#             */
-/*   Updated: 2025/03/21 11:40:01 by gcrisp           ###   ########.fr       */
+/*   Updated: 2025/03/21 11:55:35 by gcrisp           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,18 +38,17 @@ void	render_3d(t_rend *rend, t_vector *intscts, t_img *img)
 	rect_left = (img->width % intscts->length) / 2;
 	rect_width = img->width / intscts->length;
 	i = 0;
+	put_rect((t_pixel){0, 0}, (t_pixel){img->width, img->height / 2},
+		rend->ciel, img);
+	put_rect((t_pixel){0, img->height / 2},
+		(t_pixel){img->width, img->height}, rend->floor, img);
 	while (i < intscts->length)
 	{
 		intsct = ft_vecindex(intscts, i++);
 		wall_height = intsct->height * img->height;
-		put_rect((t_pixel){rect_left, 0}, (t_pixel){rect_left + rect_width,
-		(img->height - wall_height) / 2}, rend->ciel, img);
 		put_rect((t_pixel){rect_left, (img->height - wall_height) / 2},
 			(t_pixel){rect_left + rect_width,
 			(img->height + wall_height) / 2}, get_col(intsct), img);
-		put_rect((t_pixel){rect_left, (img->height + wall_height) / 2},
-			(t_pixel){rect_left + rect_width, img->height},
-			rend->floor, img);
 		rect_left += rect_width;
 	}
 }
